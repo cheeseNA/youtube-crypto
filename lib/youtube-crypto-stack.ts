f0f3212874205e558f61db355d230954c2b09277
cheeseNA <kurocat2000@gmail.com>
2022-08-11 00:40:23 +0900

    Add vpc to stack

--- lib/youtube-crypto-stack.ts
+++ lib/youtube-crypto-stack.ts
@@ -1,16 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class YoutubeCryptoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'YoutubeCryptoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'MainVpc', {
+      maxAzs: 1,
+      cidr: '10.0.0.0/16',
+      subnetConfiguration: [{
+        name: 'subnet',
+        subnetType: ec2.SubnetType.PUBLIC
+      }]
+    });
   }
 }
