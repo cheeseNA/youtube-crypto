02a1fb614d64d48a0f64d06fbd245089b1421a86
cheeseNA <kurocat2000@gmail.com>
2022-08-10 23:09:09 +0900

    Initial commit

---
+++ test/youtube-crypto.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as YoutubeCrypto from '../lib/youtube-crypto-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/youtube-crypto-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YoutubeCrypto.YoutubeCryptoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
